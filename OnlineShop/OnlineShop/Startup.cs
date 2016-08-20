583fc973f7185abb6f5036772e4f530732ae4e85
47Levan <47Vthctltc>
2016-08-20 23:14:05 +0300

    Added sign in dialog

---
+++ OnlineShop/OnlineShop/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+[assembly: OwinStartup(typeof(OnlineShop.Startup))]
+
+namespace OnlineShop
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Auth/Login")
+            });
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
